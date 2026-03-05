b0c836a8d31deb44f7a3db5d7ada51e9973bdce1
Dave Frampton <davidframpton@majicjungle.com>
2026-03-06 11:40:20 +1300

    use SDL3 main callbacks on windows

--- windows/main.cpp
+++ windows/main.cpp
@@ -1,5 +1,5 @@
 
-
+/*
 #include "MainController.h"
 #include "KatipoBrowser.h"
 
@@ -7,4 +7,55 @@ int main(int argc, const char * argv[]) {
     KatipoBrowser::getInstance()->init();
     MainController::getInstance()->runEventLoop();
     return 0;
+}*/
+#ifdef _MSC_VER
+#define _WINSOCKAPI_    // stops windows.h including winsock.h
+#define NOMINMAX
+#include <windows.h>
+#endif
+
+
+#define SDL_MAIN_USE_CALLBACKS  /* use the callbacks instead of main() */
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+
+#include "MainController.h"
+#include "KatipoBrowser.h"
+
+/* We will use this renderer to draw into this window every frame. */
+//static SDL_Window *window = NULL;
+//static SDL_Renderer *renderer = NULL;
+
+/* This function runs once at startup. */
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
+{
+    SDL_SetAppMetadata("Katipo Browser", "1.0", "com.majicjungle.katipoBrowser");
+
+    KatipoBrowser::getInstance()->init();
+
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
+
+/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
+{
+    MainController::getInstance()->handleEvent(event);
+    if(event->common.type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;
+    }
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
+
+/* This function runs once per frame, and is the heart of the program. */
+SDL_AppResult SDL_AppIterate(void* appstate)
+{
+    MainController::getInstance()->sdlLoopIterate();
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
+
+/* This function runs once at shutdown. */
+void SDL_AppQuit(void* appstate, SDL_AppResult result)
+{
+    /* SDL will clean up the window/renderer for us. */
 }
