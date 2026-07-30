65d7e4bcef85936f2586280107292f14f0d5fbeb
Dave Frampton <davidframpton@majicjungle.com>
2026-07-31 10:24:18 +1200

    fix linux waraki build, directory selection issue

--- waraki/linux/build.sh
+++ waraki/linux/build.sh
@@ -4,7 +4,9 @@ sleep 2
 if cmake --build build -- -j4; then
 ln -fTs ../../../app bin/app
 ln -fTs ../../waraki-app bin/waraki-app
-ln -fTs ../../waraki-site bin/waraki-ste
+ln -fTs ../../waraki-site bin/waraki-site
+ln -fTs ../../../katipo/apps/katipoTracker/scripts/tracker.tui bin/tracker.tui
+ln -fTs ../../../katipo/apps/katipoHost/scripts/host.tui bin/host.tui
 echo "Build complete."
 else
 exit 1;
